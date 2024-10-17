5f4af13ed3c372c330e6681f1a904b0fed1113a0
mariam-diab <mariam.diab@ejust.edu.eg>
2024-10-17 17:12:08 +0300

    Add AppointmentController

---
+++ MediCareHub/Controllers/AppointmentController.cs
@@ -0,0 +1,39 @@
+﻿using MediCareHub.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using MediCareHub.DAL.Data.Configurations;
+
+namespace MediCareHub.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AppointmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var appointments = _context.Appointments
+                                      .Select(a => new AppointmentViewModel
+                                      {
+                                          AppointmentId = a.AppointmentId,
+                                          DoctorId = a.DoctorId,
+                                          PatientId = a.PatientId,
+                                          AppointmentDate = a.AppointmentDate,
+                                          Status = a.Status,
+                                          Notes = a.Notes,
+                                          CreatedAt = a.CreatedAt
+                                      }).ToList();
+
+            return View(appointments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
